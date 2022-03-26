985add3aeefeff78f5074d225cb357b50446c688
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-03-26 21:09:21 +0600

    Error handle for making table and resolved now

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -11,32 +11,24 @@ const Question = () => {
 			</div>
 			<div className='qa'>
 				<h3 className='qa-question'>Q: 2. Differents between props and state?</h3>
-				<table>
-					<tr>
-						<th>props</th>
-						<th>state</th>
-					</tr>
-					<tr>
-						<td>1. You can read Props data.</td>
-						<td>1. You can read and write state data.</td>
-					</tr>
-					<tr>
-						<td>2. Props can not modified.</td>
-						<td>2. State can be modified.</td>
-					</tr>
-					<tr>
-						<td>3. Child component can access the props.</td>
-						<td>3. Child component can not access the state.</td>
-					</tr>
-					<tr>
-						<td>4. Props help to pass data one component to another component.</td>
-						<td>4. State Keep data about component.</td>
-					</tr>
-					<tr>
-						<td>5. By props you can reuse component.</td>
-						<td>5. By state you can not reuse component.</td>
-					</tr>
-				</table>
+				<div className='table'>
+					<div className='tr-props'>
+						<h2>props</h2>
+						<p>1. You can read Props data.</p>
+						<p>2. Props can not modified.</p>
+						<p>3. Child component can access the props.</p>
+						<p>4. Props help to pass data one component to another component.</p>
+						<p>5. By props you can reuse component.</p>
+					</div>
+					<div className='tr-state'>
+						<h2>state</h2>
+						<p>1. You can read and write state data.</p>
+						<p>2. State can be modified.</p>
+						<p>3. Child component can not access the state.</p>
+						<p>4. State Keep data about component.</p>
+						<p>5. By state you can not reuse component.</p>
+					</div>
+				</div>
 			</div>
 			<div className='qa'>
 				<h3 className='qa-question'>Q: 3. How useState works?</h3>
